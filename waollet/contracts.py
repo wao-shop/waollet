543865ffcf2c34d52a6f1a0dfd14edc0d4a46709
Dilan Nery <dnerylopes@gmail.com>
2022-02-03 00:46:00 -0300

    implement on_stake handler

--- waollet/contracts.py
+++ waollet/contracts.py
@@ -15,7 +15,30 @@ def approval_program():
         Approve(),
     )
 
-    on_stake = Approve()
+    on_stake_txn_index = Txn.group_index() - Int(1)
+    current_global_amount = App.globalGet(amount_key)
+    current_user_amount = App.localGet(Txn.sender(), staked_key)
+    on_stake = Seq(
+        Assert(
+            And(
+                Gtxn[on_stake_txn_index].type_enum() == TxnType.AssetTransfer,
+                Gtxn[on_stake_txn_index].sender() == Txn.sender(),
+                Gtxn[on_stake_txn_index].receiver()
+                == Global.current_application_address(),  # TODO should we use another address here? like an "liquidity pool"
+            )
+        ),
+        App.globalPut(
+            amount_key, current_global_amount + Gtxn[on_stake_txn_index].amount()
+        ),
+        App.localPut(
+            Txn.sender(),
+            staked_key,
+            current_user_amount + Gtxn[on_stake_txn_index].amount(),
+        ),
+        Approve(),
+    )
+
+    # TODO implement on_unstake
     on_unstake = Approve()
 
     on_call_method = Txn.application_args[0]
